ab1e58e0ccdec42a1f677298bd5e05adfc9283eb
Subhankar sarkar <81303389+S2codes@users.noreply.github.com>
2025-03-22 17:58:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'swalay-music-files.s3.ap-south-1.amazonaws.com',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'swalay-music-files.s3.ap-south-1.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    middleware: true, // Enable middleware
+  },
+  webpack: (config, { isServer }) => {
+    // This will make webpack not use eval() in production
+    if (!isServer) {
+      config.optimization.minimize = true;
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
